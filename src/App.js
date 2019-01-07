3921a24fc1d20ab4c432b89915bdc2aaf2548a12
Leandro.Carolino <carolino.leandro@gmail.com>
2019-01-07 18:55:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'read a book',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'go for a run',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'program',
+        completed: false
+      }
+      
+    ]
+  }
+  //toggle todo
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+}
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+  }
+  render() {
+    //console.log(this.state.todos) for testing purposes
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
