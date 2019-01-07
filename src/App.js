15f55d3127dff23283de76a427b4fcbd2459e72a
Leandro.Carolino <carolino.leandro@gmail.com>
2019-01-07 19:03:01 +0100

    header added(functional component)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import './App.css';
 
@@ -40,6 +41,7 @@ class App extends Component {
     //console.log(this.state.todos) for testing purposes
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
